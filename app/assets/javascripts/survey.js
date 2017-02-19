8dfc056260b16ac5bd3a7d95732c7e2912c125ff
Chad Metcalf <cm0591@gmail.com>
2017-02-18 21:51:35 -0600

    survey

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,27 +1,45 @@
-// showProgressBar: "top"
-// cookieName: "myuniquesurveyid"
-var surveyJson = { title: "Product Feedback Survey Example", pages: [
-  {questions: [
-    { type: "matrix", name: "data", title: "Please indicate if you agree or disagree with the following statements",
-      columns: [{ value: 1, text: "Strongly Disagree" },
-      { value: 2, text: "Disagree" },
-      { value: 3, text: "Neutral" },
-      { value: 4, text: "Agree" },
-      { value: 5, text: "Strongly Agree" }],
-      rows: [{ value: "affordable", text: "Product is affordable" },
-      { value: "does what it claims", text: "Product does what it claims" },
-      { value: "better then others", text: "Product is better than other products on the market" },
-      { value: "easy to use", text: "Product is easy to use" }]},
-    { type: "text", name: "email",
-      title: "Thank you for taking our survey. Your survey is almost complete, please enter your email address in the box below if you wish to participate in our drawing, then press the 'Submit' button."}
-  ]}
-]};
-var surveyData = { email: "something@example.com", data: { "better then others": "1"}};
+var surveyJson = { title: "Workshop Registration",
+  cookieName: "_workshop_survey_session",
+  showProgressBar: "top",
+  showQuestionNumbers: "false",
+  pages: [
+    { title: "How do we get a hold of you?",
+      questions: [
+      { type: "text", name: "name", isRequired: true, title: "Name"},
+      { type: "text", name: "email", isRequired: true, validators: [{type:"email"}], title: "Email"}
+    ]},
+    {
+      title: "We are looking to fill out the pilot with folks with all levels of experience.",
+      questions: [
+      { type: "matrix", name: "data", title: "What is your level of familiarity with the concept?",
+        columns: [
+        { value: 1, text: "Huh?" },
+        { value: 2, text: " " },
+        { value: 3, text: "Surface Level" },
+        { value: 4, text: " " },
+        { value: 5, text: "Evil Genius" }],
+        rows: [{ value: "refactor", text: "Refactoring" },
+        { value: "concept2", text: "Concept 2" },
+        { value: "concept3", text: "Concept 3" },
+        { value: "concept4", text: "Concept 4" }]}
+    ]}
+  ]
+};
 
+var mycustomSurveyStrings = {
+  requiredError: "required",
+  completingSurvey: "Thanks for registering! We'll have more information in the next couple of weeks.",
+};
+Survey.surveyLocalization.locales["en"] = mycustomSurveyStrings;
 Survey.defaultBootstrapCss.navigationButton = "btn btn-primary";
+Survey.defaultBootstrapCss.footer = "panel-footer text-center";
 Survey.Survey.cssType = "bootstrap";
+
 var survey = new Survey.Model(surveyJson);
+survey.locale = "en";
 survey.onComplete.add(sendDataToServer);
+survey.showTitle = false;
+survey.showPageTitles = true;
 
 var app = new Vue({
   el: '#surveyContainer',
@@ -31,14 +49,21 @@ var app = new Vue({
   methods: {
   }
 });
-survey.data = surveyData;
+
+
+// var surveyData = { email: "something@example.com", data: { "better then others": "1"}};
+// survey.data = surveyData;
 
 function sendDataToServer(survey) {
+  var surveyData = survey.data;
+  if (JSON.stringify(surveyData) === JSON.stringify({})) {
+    surveyData = {empty: true};
+  }
   $.ajax({
-      method: "POST",
-      url: "/surveys",
-      data: {'survey': survey.data}
+    method: "POST",
+    url: "/surveys",
+    data: {'survey': surveyData}
   }).done(function(html) {
-      return $("#results").append(html);
+    return $("#results").append(html);
   })
 }
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,6 +4,7 @@ class SurveysController < ApplicationController
   end
 
   def create
+    return head :unprocessable_entity if survey_params[:empty]
     @user = User.find_or_create_by(email: survey_params[:email])
     @survey = Survey.create(user: @user, data: survey_params[:data], finished_at: Time.now.utc)
   end
