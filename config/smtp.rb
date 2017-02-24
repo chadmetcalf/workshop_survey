7afc2af3b28156dcbf75fd6a49d86c2351900915
Chad Metcalf <cm0591@gmail.com>
2017-02-24 01:28:03 -0600

    change mailer

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,13 +1,13 @@
-SMTP_SETTINGS = {
-  address: ENV.fetch('MAILGUN_SMTP_SERVER'),
-  authentication: :plain,
-  domain: 'workshop-registration.heroku.com',
-  enable_starttls_auto: true,
-  password: ENV.fetch('MAILGUN_SMTP_PASSWORD'),
-  port: ENV.fetch('MAILGUN_SMTP_PORT'),
-  user_name: ENV.fetch('MAILGUN_SMTP_LOGIN'),
-  authentication: :plain
-}.freeze
+# SMTP_SETTINGS = {
+#   address: ENV.fetch('MAILGUN_SMTP_SERVER'),
+#   authentication: :plain,
+#   domain: 'workshop-registration.heroku.com',
+#   enable_starttls_auto: true,
+#   password: ENV.fetch('MAILGUN_SMTP_PASSWORD'),
+#   port: ENV.fetch('MAILGUN_SMTP_PORT'),
+#   user_name: ENV.fetch('MAILGUN_SMTP_LOGIN'),
+#   authentication: :plain
+# }.freeze
 
 if ENV['EMAIL_RECIPIENTS'].present?
   Mail.register_interceptor RecipientInterceptor.new(ENV['EMAIL_RECIPIENTS'])
