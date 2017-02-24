a6e1e6754a207aaec7c1bdc9adc8329864940237
Chad Metcalf <cm0591@gmail.com>
2017-02-24 00:59:34 -0600

    smtp settings

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 SMTP_SETTINGS = {
   address: ENV.fetch('MAILGUN_SMTP_SERVER'),
   authentication: :plain,
@@ -6,8 +5,8 @@ SMTP_SETTINGS = {
   enable_starttls_auto: true,
   password: ENV.fetch('MAILGUN_SMTP_PASSWORD'),
   port: ENV.fetch('MAILGUN_SMTP_PORT'),
-  user_name: ENV.fetch('MAILGUN_SMTP_LOGIN')
-    authentication: :plain
+  user_name: ENV.fetch('MAILGUN_SMTP_LOGIN'),
+  authentication: :plain
 }.freeze
 
 if ENV['EMAIL_RECIPIENTS'].present?
