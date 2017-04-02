51f6f3c474508c59a2589b4f5383430bb4cf799c
Chad Metcalf <cm0591@gmail.com>
2017-04-02 16:47:19 -0500

    Feedback Plan

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,37 @@
+# Allow for a user to take a series of surveys
+# depending on where they are in a process
+#
+# User can only take a type of survey one time
+#
+# current_user is kept as jwt in session cookie?
+# Use knock?
+#
+# The SurveyController determines the next survey not taken
+# SurveyController#next_survey_for(current_user)
+#
+# Ask for email address if a completed survey cookies
+# is found, but there is not a current user jwt token:
+# <--
+# You've completed the Workshop Registration Survey.
+# Please complete the Workshop 4-Week Feedback Survey.
+#
+# email: [________]
+# -->
+#
+# A User takes(has_many) :surveys
+# Survey STI with type: string
+#
+# Each type cooresponds with a SurveyTypeTemplate name:string,
+# questions: jsonb, config: jsonb
+
+# Survey#questions for the :workshop_registration type is aliases
+# to WorkshopRegistrationSurveyTemplate#questions
+#
+# Admin can see the results of different surveys
+# A sort by type admin ui (possibly with turbo links or vuejs)
+#
+# If a user has taken mutliple surveys with common questions
+# there is a way to see an overlaid radar chart of the results.
 class Survey < ApplicationRecord
   QUESTIONS = { ci: 'Continuous Integration',
                 refactor: 'Refactoring',
@@ -13,11 +47,16 @@ class Survey < ApplicationRecord
   scope :valid,    -> { where.not(data: nil) }
 
   def self.surveyjs_matrix_questions
+    # alias to template_questions?
     QUESTIONS.map do |value, text|
       { 'value': value.to_s, 'text': text.to_s }
     end
   end
 
+  # move statistics to the SurveyTypeApplicationTemplate?
+  # WorkshopRegistrationTemplate.avg
+  # Use a counter cache mechanism to maintain statistics without
+  # complex queries?
   def self.avg(key)
     return @_avg if @_avg && (@_key == key)
     @_key = key
@@ -47,6 +86,7 @@ class Survey < ApplicationRecord
   end
 
   def chart_data
+    # template_questions.chart_data
     QUESTIONS.keys.map do |key|
       data.fetch(key.to_s, "0").to_i
     end
