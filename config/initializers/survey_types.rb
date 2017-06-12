efc24fa661bf08a38ff9711931a3f6999dfc3e08
Chad Metcalf <cmetcalf@multiservice.com>
2017-06-12 13:01:44 -0500

    Add Graduation Survey to Seeds

--- config/initializers/survey_types.rb
+++ config/initializers/survey_types.rb
@@ -1,5 +1,5 @@
 if defined? Survey
-  %w(WorkshopRegistration FourWeekFeedback Null).each do |survey_type|
+  %w(WorkshopRegistration FourWeekFeedback Graduation Null).each do |survey_type|
     Survey.find_or_create_by(active: false, type: survey_type)
   end
 end
