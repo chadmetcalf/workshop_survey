d37f96577f67a58d7d8a2e3e7ef8677154c7cb2e
Chad Metcalf <cmetcalf@multiservice.com>
2017-09-29 13:09:23 -0500

    Rollbar Env Variable

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,6 +1,8 @@
 Rollbar.configure do |config|
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
   config.populate_empty_backtraces = true
   config.use_sidekiq 'queue' => 'default'
 
   config.enabled = false unless Rails.env.production?
+  config.environment = Rails.env
 end
